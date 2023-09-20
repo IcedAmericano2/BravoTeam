0442961435ed04787e1336abc311c45794cfb479
duekdms <65605368+duekdms@users.noreply.github.com>
2023-09-20 22:39:34 +0900

    management로 이름 변경

--- src/test/java/com/mju/bravoTeam/ManagementApplicationTests.java
+++ src/test/java/com/mju/bravoTeam/ManagementApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BravoTeamApplicationTests {
+class ManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
