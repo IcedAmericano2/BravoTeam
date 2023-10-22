51674f8c5d617e6cd7fecd69b7f2e50114b79ba0
dtd1614 <dtd1614@gmail.com>
2023-10-23 05:18:39 +0900

    feat: UserFeignClient로 유저 가져오기

--- src/main/java/com/mju/management/domain/project/infrastructure/Role.java
+++ src/main/java/com/mju/management/domain/project/infrastructure/Role.java
@@ -1,6 +1,11 @@
 package com.mju.management.domain.project.infrastructure;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum Role {
-    LEADER,
-    MEMBER;
+    LEADER("팀장"),
+    MEMBER("팀원");
+
+    private final String description;
 }
--- src/main/java/com/mju/management/domain/user/controller/UserFeignClient.java
+++ src/main/java/com/mju/management/domain/user/controller/UserFeignClient.java
@@ -1,12 +1,13 @@
-//package com.mju.management.domain.user.controller;
-//
-//import com.mju.management.domain.user.dto.UserInfoDto;
-//import org.springframework.cloud.openfeign.FeignClient;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//
-//@FeignClient(name = "user-service", url = "")
-//public interface UserFeignClient {
-//    @GetMapping("/response_user/{userId}")
-//    UserInfoDto getUserInfo(@PathVariable("userId") String userId);
-//}
+package com.mju.management.domain.user.controller;
+
+import com.mju.management.domain.user.dto.GetUserResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "user-service")
+public interface UserFeignClient {
+    @GetMapping("/user-service/response_user/{userId}")
+    ResponseEntity<GetUserResponseDto> getUser(@PathVariable("userId") Long userId);
+}
